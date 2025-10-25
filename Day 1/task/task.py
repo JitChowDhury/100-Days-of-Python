f9b56926a9f7c229d39f87cd2401b7ced85b5452
JitChowDhury <jitchowdhury8717@gmail.com>
2025-10-25 10:21:20 +0530

    Day 1 Completed

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Welcome to the Brand Name Generator")
+name = input("What's the name of the city you grew up in\n")
+petName = input("What's your pet's name?\n")
+print("Your brand name could be "+ name + " " + petName)
