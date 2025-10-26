e359792ef2523672f9daaa1698c545cf196ea908
JitChowDhury <jitchowdhury8717@gmail.com>
2025-10-26 15:16:22 +0530

    Day 3 Completed

---
+++ Day 3/If Else/task.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? \n"))
+
+if height<120:
+    print("You're Not Allowed Here , CHIGGA")
+else:
+    print("Goo Have fun ! Bigga")
---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,6 @@
+number = int(input("Enter Your Number: \n"))
+
+if number%2==0:
+    print("Yes It's a even Number")
+else:
+    print("No It's not a event Number")
