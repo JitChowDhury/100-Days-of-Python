80dbba1ac62cd67efeb9319e9979a6fef55d5472
JitChowDhury <jitchowdhury8717@gmail.com>
2025-10-29 13:32:25 +0530

    Day 6 Completed

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,12 @@
+name = "soumi"
+print(len(name))
+
+
+def my_function():
+    print("Hello , I want to make a fucntion ")
+    print("Bye")
+    print("----------------------------------")
+
+my_function()
+my_function()
+my_function()
