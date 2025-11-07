693c4f3702e1f2560ecc06814846b2f7a185f8cb
JitChowDhury <jitchowdhury8717@gmail.com>
2025-11-07 15:18:40 +0530

    Day 13 complete

---
+++ Day 13/Describe the Problem/task.py
@@ -0,0 +1,15 @@
+def my_function():
+    for i in range(1, 20):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
+
+# Describe the Problem - Write your answers as comments:
+# 1. What is the for loop doing?
+#The for loop is ruunning from 1 to 19
+# 2. When is the function meant to print "You got it"?
+#Never
+# 3. What are your assumptions about the value of i?
+#1 to 19
