19fcb103e0e063ab4ea5154a4a8848928069a978
JitChowDhury <jitchowdhury8717@gmail.com>
2025-11-19 11:31:01 +0530

    Day 16 complete

---
+++ Day 16/day-16-start/main.py
@@ -0,0 +1,28 @@
+# # # import turtle
+# # # Helifakx=turtle.Turtle()
+# #
+# # from turtle import  Turtle , Screen
+# # Helifakx=Turtle()
+# #
+# # print(Helifakx)
+# # Helifakx.shape("turtle")
+# # Helifakx.color("red")
+# # Helifakx.forward(100)
+# #
+# # my_screen = Screen()
+# #
+# # my_screen.canvwidth=800
+# # my_screen.canvheight=600
+# #
+# # my_screen.exitonclick()
+#
+# from prettytable import  PrettyTable
+#
+# table = PrettyTable()
+# print(table)
+#
+# table.add_column("Pokemon name:" , ["Pikachu","Squirte","Charmander"])
+# table.add_column("Type:" , ["Electric","Water","Fire"])
+# table.align= "l"
+# print(table)
+
