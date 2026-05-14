08c1cfbc472d7c9632d9dad27fc133e2bb51f977
JitChowDhury <jitchowdhury8717@gmail.com>
2026-05-14 08:55:14 +0530

    Day 20 Done

---
+++ day-20/snake.py
@@ -0,0 +1,44 @@
+from turtle import Turtle,Screen
+
+class Snake:
+  
+  def __init__(self):
+    self.snake_segments=[]
+    self.create_snake()
+    self.angle=0
+
+  def create_snake(self):
+    self.xPos=0
+    self.YPos=0
+    for _ in range(3):
+      snake=Turtle(shape="square")
+      snake.color("white")
+      snake.penup()
+      snake.setposition(self.xPos,self.YPos)
+      self.snake_segments.append(snake)
+      self.xPos-=20
+
+  def set_angle(self,angle):
+    self.angle=angle
+
+
+  def go_up(self):
+    if self.snake_segments[0].heading()!=270:
+      self.set_angle(90)
+  def go_down(self):
+    if self.snake_segments[0].heading()!=90:
+      self.set_angle(270)
+  def go_right(self):
+    if self.snake_segments[0].heading()!=180:
+      self.set_angle(0)
+  def go_left(self):
+    if self.snake_segments[0].heading()!=0:    
+      self.set_angle(180)
+
+  def move(self):
+    for seg_num in range(len(self.snake_segments)-1,0,-1):
+      newX=self.snake_segments[seg_num-1].xcor()
+      newY=self.snake_segments[seg_num-1].ycor()
+      self.snake_segments[seg_num].goto(newX,newY)
+    self.snake_segments[0].setheading(self.angle)
+    self.snake_segments[0].forward(20)
---
+++ day-20/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+screen.onkey(snake.set_angle(180),"S")
+screen.onkey(snake.set_angle(90),"D")
+screen.onkey(snake.set_angle(270),"A")
