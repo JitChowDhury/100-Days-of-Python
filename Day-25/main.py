455ac0af4e5500b25953967e29ec0c5b2e2d3f26
JitChowDhury <jitchowdhury8717@gmail.com>
2026-05-22 22:49:16 +0530

    basic pandas

--- Day-25/main.py
+++ Day-25/main.py
@@ -10,10 +10,52 @@
 
 # print(temperatures)
 
-import pandas
+# import pandas
 
-data = pandas.read_csv("Day-25/weather_data.csv")
-print(data)
+# data = pandas.read_csv("Day-25/weather_data.csv")
+# print(data)
+# print(type(data))
+# temp= data.temp.to_list()
+# print(temp)
 
-temp = data["temp"].to_list();
-print(sum(temp)/len(temp))
+# maxTemp=data.temp.max()
+# print(maxTemp)
+
+
+# print(data[data.day=="Monday"])
+# print(data[data.temp==data.temp.max()])
+
+# def celToF(num):
+#   return ((num*1.8)+32)
+
+# print(celToF(data[data.day=="Monday"].temp))
+
+
+# #create dataFrame from scratch
+# data_dict = {
+#   "students":["Amy","James","Angela"],
+#   "scores":[76,56,65]
+# }
+
+# scoreData=pandas.DataFrame(data_dict)
+# print(scoreData)
+# scoreData.to_csv("Day-25/score.csv")
+
+import pandas as pd
+print(pd.__version__)
+data=[902,903,904,109,70]
+room_series=pd.Series(data,index=["Room1","Room2","Room3","Room4","Room5"])
+room_series.loc["Room1"]=9902
+room_series.iloc[1]=2909
+# print(room_series)
+
+
+calories = { "Day 1":1750,"Day 2":2025,"Day 3":1500}
+calorie_series=pd.Series(calories)
+print(calorie_series)
+
+# print(calorie_series[calorie_series<2000])
+
+data={
+  "Name":["SpongeBob","Patrick","Squidward"]
+}
