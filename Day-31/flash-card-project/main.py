ae443b515273357e66c6831c349cea856e430cdc
JitChowDhury <jitchowdhury8717@gmail.com>
2026-06-18 15:03:21 +0530

    Day 31 Completed

--- Day-31/flash-card-project/main.py
+++ Day-31/flash-card-project/main.py
@@ -1,24 +1,70 @@
 from tkinter import *
-BACKGROUND_COLOR = "#B1DDC6"
+import pandas
+import random
 
-window = Tk();
+BACKGROUND_COLOR = "#B1DDC6"
+current_card={}
+to_learn={}
+
+try:
+  data=pandas.read_csv("E:/CODE/100 days of Code-Python/Day-31/flash-card-project/data/words_to_learn.csv")
+except FileNotFoundError:
+  original_data=pandas.read_csv("E:/CODE/100 days of Code-Python/Day-31/flash-card-project/data/french_words.csv")
+  to_learn=original_data.to_dict(orient="records")
+else:
+  to_learn=data.to_dict(orient="records")
+
+
+def next_card():
+  global current_card
+  global flip_timer
+  window.after_cancel(flip_timer)
+  current_card=random.choice(to_learn)
+  canvas.itemconfig(card_title,text="French",fill="black")
+  canvas.itemconfig(card_word,text=current_card["French"],fill="black")
+  canvas.itemconfig(card_background,image=card_front_img)
+  flip_timer=window.after(3000,func=flip_card)
+
+
+
+
+def flip_card():
+  canvas.itemconfig(card_title,text="English",fill="white")
+  canvas.itemconfig(card_word,text=current_card["English"],fill="white")
+  canvas.itemconfig(card_background,image=card_back_img)
+
+def is_known():
+  to_learn.remove(current_card)
+
+  data=pandas.DataFrame(to_learn)
+  data.to_csv("E:/CODE/100 days of Code-Python\Day-31\words_to_learn.csv",index=False)
+
+
+  next_card()
+
+window = Tk()
 window.title("Flashy")
 window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
 
+flip_timer=window.after(3000,func=flip_card)
+
 canvas=Canvas(width=800,height=526,bg=BACKGROUND_COLOR,highlightthickness=0)
 card_front_img=PhotoImage(file="E:/CODE/100 days of Code-Python\Day-31/flash-card-project/images/card_front.png")
-canvas.create_image(400,263,image=card_front_img)
-canvas.create_text(400,150,text="Title",font=("Ariel",40,"italic"))
-canvas.create_text(400,263,text="word",font=("Ariel",60,"bold"))
+card_back_img=PhotoImage(file="E:/CODE/100 days of Code-Python\Day-31/flash-card-project/images/card_back.png")
+card_background=canvas.create_image(400,263,image=card_front_img)
+card_title=canvas.create_text(400,150,text="Title",font=("Ariel",40,"italic"))
+card_word=canvas.create_text(400,263,text="word",font=("Ariel",60,"bold"))
 
 canvas.grid(row=0,column=0,columnspan=2)
 
 
 cross_image=PhotoImage(file="E:/CODE/100 days of Code-Python\Day-31/flash-card-project/images/wrong.png")
-unknown_button=Button(image=cross_image,highlightthickness=0)
+unknown_button=Button(image=cross_image,highlightthickness=0,command=next_card)
 unknown_button.grid(row=1,column=0)
 
 check_image=PhotoImage(file="E:/CODE/100 days of Code-Python\Day-31/flash-card-project/images/right.png")
-known_button=Button(image=check_image,highlightthickness=0)
+known_button=Button(image=check_image,highlightthickness=0,command=is_known)
 known_button.grid(row=1,column=1)
+
+next_card()
 window.mainloop()
